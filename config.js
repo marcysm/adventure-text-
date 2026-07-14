b69e8ad07a5e195926ee18a14ed035b294d092f1
marcysm <marcysm30@gmail.com>
2026-07-14 13:42:45 -0300

    Adicionar configuração do Supabase

---
+++ config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+  CONFIGURAÇÃO PÚBLICA DO SUPABASE
+
+  A Project URL e a Publishable Key podem ser utilizadas
+  no navegador porque a segurança dos dados será controlada
+  pelas políticas RLS do banco.
+
+  Nunca coloque aqui:
+  - Secret Key
+  - service_role
+  - senha do banco
+*/
+
+window.APP_CONFIG = {
+  supabaseUrl: "https://kigiofnknaqkxqpcaimy.supabase.co",
+
+  supabasePublishableKey: "sb_publishable_bpe6CO0n5BLaTJdQZHvoXw_gLGkmvFH"
+};
